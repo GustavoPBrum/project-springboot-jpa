5c5eda71a5d09eae04e305cfd2fbf72ec60c5b43
Gustavo Pereira Brum <brumtavo@gmail.com>
2025-05-10 21:13:19 +0100

    User entity and resource

---
+++ src/main/java/com/estudandobackend/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.estudandobackend.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudandobackend.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+    @GetMapping  // responde a requisicao do tipo GET do HTTP
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Junim", "junim@hotmail.com", "9301283", "1234");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
