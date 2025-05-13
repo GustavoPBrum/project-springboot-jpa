e1ce2ffdbaa45c802d575475ee9b07630269f9cc
Gustavo Pereira Brum <brumtavo@gmail.com>
2025-05-13 02:02:08 +0100

    Service layer, component registration

--- src/main/java/com/estudandobackend/course/resources/UserResource.java
+++ src/main/java/com/estudandobackend/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.estudandobackend.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudandobackend.course.entities.User;
+import com.estudandobackend.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
+	
     @GetMapping  // responde a requisicao do tipo GET do HTTP
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Junim", "junim@hotmail.com", "9301283", "1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {  // Corpo da resposta vai ser uma lista de usuarios
+		List<User> list = service.findAll();
 		
+		return ResponseEntity.ok().body(list);  // Body -> conteudo da resposta, no caso o objeto list no qual contem lista de users
 	}
+    
+    @GetMapping(value = "/{id}")  // Indica que a requisicao vai aceitar um id dentro da URL
+    public ResponseEntity<User> findById(@PathVariable Long id) {  // Para aceitar o id e considera-lo como parametro que chegara na URL
+    	User obj = service.findById(id);
+    	
+    	return ResponseEntity.ok().body(obj);
+    }
 }
---
+++ src/main/java/com/estudandobackend/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.estudandobackend.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudandobackend.course.entities.User;
+import com.estudandobackend.course.repositories.UserRepository;
+
+@Service  // Registrando como um componente Spring para ser automaticamente injetadoe deixando mais semanticamente correto e especifico
+public class UserService {
+
+	@Autowired  // O servico faz a chamada com o banco de dados (UserRepository)
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();  // Operacao padrao ja do UserRepository padrao do Spring
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		 
+		return obj.get();  // A operacao 'get' do optional vai retornar o objeto do tipo entre <> (no caso User) que estiver dentro de *obj*
+	}
+
+}
